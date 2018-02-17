d1bf7e157893d745a7000b598624673239bc555e
KaiN <kain@piwnica.ws>
2018-02-17 11:29:15 +0100

    Fixed codestyle

--- main.js
+++ main.js
@@ -1,122 +1,111 @@
-function tCycle(isAbleBitplane, isAbleCopperWait, isAbleCopperSet) {
+function tCycle(isAbleBitplane, isAbleCopperWait) {
 	this.isAbleBitplane = isAbleBitplane;
 	this.isAbleCopperWait = isAbleCopperWait;
-	this.isAbleCopperSet = isAbleCopperSet;
 }
 
-function tDma(bpp) {
+function tDma(nBpp) {
 	// DMA may span across many frames, but let's ignore it for now
 	// There are 0xE4 cycles on row, and AFAIR 312 cycle rows.
-	var ddfstrt = {x: 0x38, y: 0x2C}; // display datafetch start - custom reg
-	var ddfstop = {x: 0xD0, y: 0x2C + 256}; // display datafetch stop
+	var ddfstrt = {nX: 0x38, nY: 0x2C}; // display datafetch start - custom reg
+	var ddfstop = {nX: 0xD0, nY: 0x2C + 256}; // display datafetch stop
 	this.cyclesInRow = 0xE4;
-	this.cycleRows = 312;
-	this.slots = [];
-	this.bpp = bpp;
+	this.nCycleRows = 312;
+	this.pSlots = [];
+	this.nBpp = nBpp;
 
 	// Lores bitplane fetch scheme - 0 is free, first bitplane is 1
 	var bplFetchSchemes = [0, 4, 6, 2, 0, 3, 5, 1]; // 6bpp
 
 	for (var c = 0; c < this.cyclesInRow; ++c) {
-		this.slots[c] = [];
-		for (var r = 0; r < this.cycleRows; ++r) {
+		this.pSlots[c] = [];
+		for (var r = 0; r < this.nCycleRows; ++r) {
 			// Determine for what given cycle may be used
 			var f = bplFetchSchemes[c % 8];
-			var isAbleBitplane = (c > ddfstrt.x) &&
-				(c < ddfstop.x + bplFetchSchemes.length) &&
-				(r > ddfstrt.y) && (r < ddfstop.y) &&
-				f && (f <= bpp);
+			var isAbleBitplane = (c > ddfstrt.nX) &&
+				(c < ddfstop.nX + bplFetchSchemes.length) &&
+				(r > ddfstrt.nY) && (r < ddfstop.nY) &&
+				f && (f <= nBpp);
 			var isAbleCopperWait = !(c & 1);
-			var isAbleCopperSet = isAbleCopperWait && !isAbleBitplane;
-			this.slots[c][r] = new tCycle(
-				isAbleBitplane, isAbleCopperWait, isAbleCopperSet
-			);
+			this.pSlots[c][r] = new tCycle(isAbleBitplane, isAbleCopperWait);
 		}
 	}
 }
 
-function tDmaVisualizer(canvas, dma) {
-	this.canvas = canvas;
-	this.pixelSize = 3;
-	this.dmaSpacing = 1;
-	this.dma = dma;
+function tDmaVisualizer(eCanvas, Dma) {
+	this.eCanvas = eCanvas;
+	this.nPixelSize = 3;
+	this.nDmaSpacing = 1;
+	this.Dma = Dma;
 
-	var dmaWidth = 2 * this.pixelSize;
-	this.canvasLimits = {
-		w: this.dmaSpacing
-			+ this.dma.cyclesInRow * (this.dmaSpacing + dmaWidth),
-		h: this.dmaSpacing
-			+ this.dma.cycleRows * (this.dmaSpacing + this.pixelSize)
-	}
-	this.ctx = canvas.getContext('2d');
+	this.Ctx = eCanvas.getContext('2d');
 
-	canvas.visualizer = this;
-	canvas.beginPos = {x: 0, y: 0};
-	canvas.scale = 1.0;
-	canvas.width = 640;
-	canvas.height = 256;
+	eCanvas.visualizer = this;
+	eCanvas.BeginPos = {nX: 0, nY: 0};
+	eCanvas.nScale = 1.0;
+	eCanvas.width = 640;
+	eCanvas.height = 256;
 
-	canvas.onmouseup = canvas.onmouseout = function(e) {
+	eCanvas.onmouseup = eCanvas.onmouseout = function(Evt) {
 		this.isDragged = false;
 	}
 
-	canvas.onmousedown = function(e) {
+	eCanvas.onmousedown = function(Evt) {
 		this.isDragged = true;
-		this.dragStart = {
-			x: e.offsetX, y: e.offsetY
+		this.DragStart = {
+			nX: Evt.offsetX, nY: Evt.offsetY
 		};
 	}
 
-	canvas.onmousemove = function(e) {
+	eCanvas.onmousemove = function(Evt) {
 		if(this.isDragged) {
-			this.beginPos.x += e.offsetX - this.dragStart.x;
-			this.beginPos.y += e.offsetY - this.dragStart.y;
+			this.BeginPos.nX += Evt.offsetX - this.DragStart.nX;
+			this.BeginPos.nY += Evt.offsetY - this.DragStart.nY;
 
-			this.dragStart.x = e.offsetX;
-			this.dragStart.y = e.offsetY;
+			this.DragStart.nX = Evt.offsetX;
+			this.DragStart.nY = Evt.offsetY;
 		}
 	}
 
-	canvas.onwheel = function(e) {
+	eCanvas.onwheel = function(Evt) {
 		// Get new scale
-		var clamp = function(x, bottom, top) {return Math.min(Math.max(x, bottom), top);};
-		this.scale = clamp(this.scale - Math.sign(e.deltaY), 1, 20);
+		var clamp = function(nX, bottom, top) {return Math.min(Math.max(nX, bottom), top);};
+		this.nScale = clamp(this.nScale - Math.sign(Evt.deltaY), 1, 20);
 
 		// Get currently hovered cycle
-		var mousePos = {x: e.offsetX, y: e.offsetY};
-		var cycle = this.visualizer.getCycleXyFromScreenPos(mousePos);
+		var mousePos = {nX: Evt.offsetX, nY: Evt.offsetY};
+		var Cycle = this.visualizer.getCycleXyFromScreenPos(mousePos);
 
 		// Scale view
-		this.visualizer.pixelSize = this.scale;
+		this.visualizer.nPixelSize = this.nScale;
 
 		// Adjust scroll offsets so that hover will be on same cycle
-		var absPos = this.visualizer.getAbsPosFromCycleXy(cycle);
-		this.beginPos.x = Math.round(mousePos.x - absPos.x);
-		this.beginPos.y = Math.round(mousePos.y - absPos.y);
+		var AbsPos = this.visualizer.getAbsPosFromCycleXy(Cycle);
+		this.BeginPos.nX = Math.round(mousePos.nX - AbsPos.nX);
+		this.BeginPos.nY = Math.round(mousePos.nY - AbsPos.nY);
 
 		return false;
 	}
 }
 
-tDmaVisualizer.prototype.getCycleXyFromScreenPos = function(pos) {
+tDmaVisualizer.prototype.getCycleXyFromScreenPos = function(Pos) {
 	return this.getCycleXyFromAbsPos({
-		x: pos.x - this.canvas.beginPos.x,
-		y: pos.y - this.canvas.beginPos.y
+		nX: Pos.nX - this.eCanvas.BeginPos.nX,
+		nY: Pos.nY - this.eCanvas.BeginPos.nY
 	});
 }
-tDmaVisualizer.prototype.getCycleXyFromAbsPos = function(pos) {
-	var dmaSize = this.pixelSize * 2;
+tDmaVisualizer.prototype.getCycleXyFromAbsPos = function(Pos) {
+	var nDmaSize = this.nPixelSize * 2;
 	return {
-		x: (pos.x - this.dmaSpacing) / (this.dmaSpacing + dmaSize),
-		y: (pos.y - this.dmaSpacing) / (this.dmaSpacing + this.pixelSize)
+		nX: (Pos.nX - this.nDmaSpacing) / (this.nDmaSpacing + nDmaSize),
+		nY: (Pos.nY - this.nDmaSpacing) / (this.nDmaSpacing + this.nPixelSize)
 	};
 }
 
-tDmaVisualizer.prototype.getAbsPosFromCycleXy = function(cycle) {
-	var dmaSize = this.pixelSize * 2;
+tDmaVisualizer.prototype.getAbsPosFromCycleXy = function(CyclePos) {
+	var nDmaSize = this.nPixelSize * 2;
 	return {
-		x: this.dmaSpacing + (this.dmaSpacing + dmaSize) * cycle.x,
-		y: this.dmaSpacing + (this.dmaSpacing + this.pixelSize) * cycle.y
+		nX: this.nDmaSpacing + (this.nDmaSpacing + nDmaSize) * CyclePos.nX,
+		nY: this.nDmaSpacing + (this.nDmaSpacing + this.nPixelSize) * CyclePos.nY
 	};
 }
 
@@ -124,53 +113,62 @@ tDmaVisualizer.prototype.drawGrid = function () {
 	if(this.isDrawing) {
 		return;
 	}
-	var dmaSize = this.pixelSize * 2;
+	var nDmaSize = this.nPixelSize * 2;
 	this.isDrawing = true;
 
 	// BG
-	this.ctx.fillStyle = '#000';
-	this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+	this.Ctx.fillStyle = '#000';
+	this.Ctx.fillRect(0, 0, this.eCanvas.width, this.eCanvas.height);
 
 	// Draw cycle grid
-	for (var y = 0; y < this.dma.cycleRows; ++y) {
-		var offsetY = this.canvas.beginPos.y + this.dmaSpacing + (this.dmaSpacing + this.pixelSize) * y;
-		if(offsetY + (2 * this.dmaSpacing + this.pixelSize) < 0) {
+	for (var nY = 0; nY < this.Dma.nCycleRows; ++nY) {
+		var nOffsetY = this.eCanvas.BeginPos.nY + this.nDmaSpacing + (this.nDmaSpacing + this.nPixelSize) * nY;
+		if(nOffsetY + (2 * this.nDmaSpacing + this.nPixelSize) < 0) {
 			continue;
 		}
-		if(offsetY > this.canvas.height) {
+		if(nOffsetY > this.eCanvas.height) {
 			break;
 		}
-		for (var x = 0; x < this.dma.cyclesInRow; ++x) {
-			var offsetX = this.canvas.beginPos.x + this.dmaSpacing + (this.dmaSpacing + dmaSize) * x;
-			if(offsetX + (2 * this.dmaSpacing + dmaSize) < 0) {
+		for (var nX = 0; nX < this.Dma.cyclesInRow; ++nX) {
+			var nOffsetX = this.eCanvas.BeginPos.nX + this.nDmaSpacing + (this.nDmaSpacing + nDmaSize) * nX;
+			if(nOffsetX + (2 * this.nDmaSpacing + nDmaSize) < 0) {
 				continue;
 			}
-			if(offsetX > this.canvas.width) {
+			if(nOffsetX > this.eCanvas.width) {
 				break;
 			}
-			if(this.dma.slots[x][y].isAbleBitplane) {
+			if(this.Dma.pSlots[nX][nY].isAbleBitplane) {
 				// Bitplane DMA
-				this.ctx.fillStyle = '#00F';
+				this.Ctx.fillStyle = '#00F';
 			}
-			else if(this.dma.slots[x][y].isAbleCopperSet){
-				this.ctx.fillStyle = '#F80';
+			else if(this.Dma.pSlots[nX][nY].isAbleCopperWait){
+				this.Ctx.fillStyle = '#F80';
 			}
 			else {
-				this.ctx.fillStyle = '#FFF';
+				this.Ctx.fillStyle = '#FFF';
 			}
-			this.ctx.fillRect(offsetX, offsetY, dmaSize, this.pixelSize);
+			this.Ctx.fillRect(nOffsetX, nOffsetY, nDmaSize, this.nPixelSize);
 		}
 	}
 	this.isDrawing = false;
 }
 
-function main() {
-	var bpp = 3;
+/**
+ * Prefixes:
+ * tName - class
+ * nName - number
+ * sName - string
+ * eName - element
+ * isName - bool
+ * pName - array
+ * Name - other object
+ */
 
-	var canvas = document.querySelector('#dmaVisualizer');
-	var dma = new tDma(bpp);
-	var dmaVisualizer = new tDmaVisualizer(canvas, dma);
-	setInterval(function() {dmaVisualizer.drawGrid()}, 1/30);
-}
+window.addEventListener('load', function() {
+	var nBpp = 3;
 
-window.addEventListener('load', main);
+	var eCanvas = document.querySelector('#dmaVisualizer');
+	var Dma = new tDma(nBpp);
+	var DmaVisualizer = new tDmaVisualizer(eCanvas, Dma);
+	setInterval(function() {DmaVisualizer.drawGrid()}, 1/30);
+});
